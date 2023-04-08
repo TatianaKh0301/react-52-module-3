4c282297223bf2ee11596f9c8dac299c4651191d
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-04-08 16:00:35 +0300

    add axios, react-select

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import axios from 'axios';
+import Select from 'react-select'
+
+
+axios.default.baseURL = 'https://api.thedogapi.com/v1';
+axios.default.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+export class App extends Component {
+    render() {
+        return (
+            <>
+              <Select options={options} />
+            </>
+        );
+  } 
 };
